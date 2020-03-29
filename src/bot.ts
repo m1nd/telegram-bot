083678039bcbfa05bb40586367d2e9dd967e69f9
m1nd <m1nd@mail.ru>
2020-03-29 12:36:11 +0300

    ts Add axios

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,6 @@
 import * as Bot from 'node-telegram-bot-api';
+// import * as axios from 'axios';
+import axios from 'axios';
 
 import shapeTypeDetermination from "./utils/newDetermination";
 import {
@@ -33,6 +35,8 @@ let clientStore = {};
 let bot;
 
 const link = 'https://telegra.ph/Sample-Page-03-21-32';
+
+const getLink = `https://api.telegra.ph/createPage?access_token=b968da509bb76866c35425099bc0989a5ec3b32997d55286c657e6994bbb&title=Sample+Page&author_name=Anonymous&content=[{"tag":"p","children":["Hello,+tadam!!!"]}]&return_content=true`;
 
 if (process.env.NODE_ENV === 'production') {
   bot = new Bot(token);
@@ -154,6 +158,13 @@ bot.on('callback_query', callbackQuery => {
                                 scaleOfFat: +scaleOfFatMsg.text,
                               };
                               bot.sendMessage(scaleOfFatMsg.from.id, link);
+                              axios.get(getLink)
+                              .then((res) => {
+                                console.log(`content => : ${res}`)
+                              })
+                              .catch((error) => {
+                                console.error(error)
+                              });
                               console.log('STart =>>>', scaleOfFatMsg.from.id);
                               const shape = shapeTypeDetermination(clientStore[scaleOfFatMsg.from.id]);
                               const user = new User({
