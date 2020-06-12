58d5b33edf4c5d5e1e6c4fa3b4c58838a1540a23
m1nd <m1nd@mail.ru>
2020-06-12 14:32:23 +0300

    ts: Up

--- src/bot.ts
+++ src/bot.ts
@@ -59,7 +59,7 @@ const makeGetRequest = async (content: string): Promise<any> => {
         {
           tag: 'img',
           attrs: {
-            src: 'src/assets/metrics.png',
+            src: 'assets/figure_type.jpg',
             // width: 100,
             // height: 100,
             // src: "https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg", 
