38142a7a92cb392996e738ff1525d25f331c73b0
m1nd <m1nd@mail.ru>
2020-06-12 14:08:38 +0300

    ts: Up

--- src/bot.ts
+++ src/bot.ts
@@ -59,9 +59,11 @@ const makeGetRequest = async (content: string): Promise<any> => {
         {
           tag: 'img',
           attrs: {
-            src: 'assets/metrics.png',
-            width: 100,
-            height: 100,
+            // src: 'assets/metrics.png',
+            // width: 100,
+            // height: 100,
+            src: "https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg", 
+            alt: "no image"
           }
         },
         {
